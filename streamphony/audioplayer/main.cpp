2d5a8ebd772a60672f6db3dcf9439fe48787ac45
Balazs Bela <balazsbela@gmail.com>
2014-05-29 18:25:25 +0300

    Minor port adjustment

--- streamphony/audioplayer/main.cpp
+++ streamphony/audioplayer/main.cpp
@@ -7,8 +7,6 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    //MediaStream mediaStream
-
     PhononMediaPlayer player;
     player.play(QStringLiteral("http://79.116.48.163:45172/1.mp3"));
 
