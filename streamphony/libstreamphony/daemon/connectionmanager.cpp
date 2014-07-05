92fd1442705a1c7e66c83993d4948501bd87559c
Balazs Bela <balazsbela@gmail.com>
2014-07-05 13:31:48 +0300

    Revert "Query indefinitely"
    This reverts commit 7f6b6c91d3a64f089fdc0fe9cebdd93e45bc66df.

--- streamphony/libstreamphony/daemon/connectionmanager.cpp
+++ streamphony/libstreamphony/daemon/connectionmanager.cpp
@@ -48,8 +48,7 @@ void ConnectionManager::populateNodeHash()
 
         if (nodeIdMap[hash] != nullptr && nodeIdMap[hash]->jid() == jid) {
             if (nodeIdMap[hash]->queryRunning()) {
-                // TODO: UNCOMMENT
-                //        return;
+                return;
             }
         }
         debugConnectionManager() << "Launching ip query for:" << jid << ":" << m_xmppManager->fullName(jid) << hash;
